99110011cf19a9e88698b4e70aac27b5b6c602a4
dennis gega <dennis.gega@uconn.edu>
2025-03-23 18:32:39 -0400

    finalized error handling

--- part4/notes-backend/index.js
+++ part4/notes-backend/index.js
@@ -45,21 +45,19 @@ app.delete('/api/notes/:id', (request, response) => {
         .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({ error: 'content missing' })
-    }
 
     const note = new Note({
         content: body.content,
         important: body.important || false,
     })
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (req, res, next) => {
@@ -91,7 +89,9 @@ const errorHandler = (error, req, res, next) => {
     console.error(error.message)
 
     if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
+        return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message })
     }
 
     next(error)
