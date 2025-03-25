e824aa63dc4cf176d5998564e97b4678f49ee2d3
dennis gega <dennis.gega@uconn.edu>
2025-03-24 21:19:58 -0400

    changed folder locations

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,102 +1,55 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(express.static('dist'))
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (req, res) => {
-    res.json(persons);
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-
-    const person = persons.find(person => person.id === id);
-    if (person) {
+    Person.findById(req.params.id).then(person => {
         res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    })
 })
 
-app.get('/info', (req, res) => {
-    const date = new Date();
-    res.send(`
-        <div>
-            <p>Phonebook has info for ${persons.length} people</p>
-        </div>
-        <div>
-            <p>${date}</p>
-        </div>
-        `)
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id)
+        .then(res => {
+            res.status(204).end()
+        })
+        .catch(err => next(err))
 })
-
-const generateId = () => {
-    return String(Math.floor(Math.random() * 777))
-}
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
 
-    if (!body) {
-        return res.status(404).json({
-            error: 'content missing'
+    if (!body.name || !body.number) {
+        return res.status(400).json({ error: 'Name or number missing' });
+    }
+
+    Person.findOne({number: body.number}).then(result => {
+        if (result)
+            return res.status(400).json({ error: 'Number already registered' })
+
+        const person = new Person({
+            name: body.name,
+            number: body.number
         })
-    }
 
-    if (!body.name) {
-        return res.status(400).json({
-            error: 'Name missing'
+        person.save().then(savedPerson => {
+            res.json(savedPerson)
         })
-    }
-
-    if (!body.number) {
-        return res.status(400).json({
-            error: 'Number missing'
-        })
-    }
-
-    if (persons.find(person => person.name === body.name)) {
-        return res.status(400).json({
-            error: `${body.name} is already registered`
-        })
-    }
-
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId()
-    }
-
-    persons = persons.concat(person)
-        
-    res.json(person)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+
+const PORT = process.env.PORT
 app.listen(PORT)
